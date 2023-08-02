69b5920f8eb69dbe6842ff256e1ebe3c37042a88
maarish441-crypto <maarish441@gmail.com>
2023-08-03 04:32:19 +0530

    requiements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages , setup
+
+setup(
+    name = "cell segmentation",
+    version = "0.0.0",
+    author= "M.Aarish",
+    author_email = "maarish441@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
